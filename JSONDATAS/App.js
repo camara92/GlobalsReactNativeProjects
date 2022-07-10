4d930d14315574271f373ab5418e5133661053d0
camara92 <88945535+camara92@users.noreply.github.com>
2022-07-11 01:00:43 +0200

    utilisation SectionList ==>affichage des datas

---
+++ JSONDATAS/App.js
@@ -0,0 +1,21 @@
+// Exercice 1
+import React from "react";
+import { View } from "react-native";
+
+export default function App() {
+
+  const obj = [
+    {id: "1", name: "Stan", age: 45},
+    {id: "2", name: "Francine", age: 45},
+    {id: "3", name: "hayley", age: 18},
+    {id: "4", name: "Steve", age: 14},
+    {id: "5", name: "Roger", age: 1020},
+    {id: "6", name: "Klaus", age: 30},
+  ]
+
+  return (
+    <View></View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ JSONDATAS/array.js
@@ -0,0 +1,26 @@
+const obj = [
+  {
+    role: "Père",
+    data: ["Stan", 45]
+  },
+  {
+    role: "Mère",
+    data: ["Francine", 45]
+  },
+  {
+    role: "Fille",
+    data: ["hayley", 18]
+  },
+  {
+    role: "Fils",
+    data: ["Steve", 14]
+  },
+  {
+    role: "Alien",
+    data: ["Roger", 1020]
+  },
+  {
+    role: "Poisson",
+    data: ["Klaus", 30]
+  },
+]
